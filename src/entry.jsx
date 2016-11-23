c827e2061ab567e94e77491c2ee3ff119159daf5
Ben Gummer <bgummer@atlassian.com>
2016-11-23 11:04:27 +1100

    chore: remove polyfills for now

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'skatejs-web-components'; // polyfills
 import App from './modules/App.jsx';
 
 // eslint-disable-next-line no-undef
