7d5f7e7d969a2823889d3372df14d09a79dd94a9
Ben Gummer <bgummer@atlassian.com>
2016-11-28 09:11:37 +1100

    chore: use Reactified navigation component

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -1,59 +1,59 @@
 import React, { PureComponent } from 'react';
-import PageComponent from 'ak-page';
-import Navigation, { NavigationLink } from 'ak-navigation';
+import Nav, { AkContainerItem, AkContainerHeader as NavHeader } from 'ak-navigation';
 import { Link } from 'react-router';
 
-import AtlassianIconWC from 'ak-icon/glyph/atlassian';
-import GearIconWC from 'ak-icon/glyph/bitbucket/admin';
-import DashboardIconWC from 'ak-icon/glyph/bitbucket/dashboard';
-import PullRequestsIconWC from 'ak-icon/glyph/bitbucket/pullrequests';
+import {
+  AtlassianIcon,
+  BitbucketAdminIcon as GearIcon,
+  BitbucketDashboardIcon as DashboardIcon,
+  BitbucketPullrequestsIcon as PullRequestsIcon,
+} from 'ak-icon';
 import 'ak-css-reset';
 
-// Here we convert our skatejs web components into React components using 'reactify'
-// In a few weeks these components will all be native React components so we will be
-// able to get rid of these 'reactify' calls.
-import reactify from 'skatejs-react-integration'; // eslint-disable-line
-const Page = reactify(PageComponent, {});
-const Nav = reactify(Navigation, {});
-const NavLink = reactify(NavigationLink, {});
-const AtlassianIcon = reactify(AtlassianIconWC, {});
-const GearIcon = reactify(GearIconWC, {});
-const DashboardIcon = reactify(DashboardIconWC, {});
-const PullRequestsIcon = reactify(PullRequestsIconWC, {});
+const myLinks = [
+  ['/', 'Home'],
+  ['/pull-requests', 'Pull requests'],
+  ['/settings', 'Settings'],
+];
 
 export default class App extends PureComponent {
   render() {
     return (
-      <Page layout="fixed">
+      <div
+        style={{
+          display: 'flex',
+          height: '100vh',
+          overflowY: 'auto',
+        }}
+      >
+        <style>{'body { margin: 0 }'}</style>
         <Nav
-          open
-          containerName="My app"
-          containerHref="http://example.com"
-          containerLogo="nucleus.png"
-          productHref="http://atlassian.design"
-          collapsible
-          slot="navigation"
+          containerHeader={
+            <Link to="/">
+              <NavHeader
+                text={'AtlasCat'}
+                icon={<img alt="nucleus" src="nucleus.png" />}
+              />
+            </Link>
+          }
         >
-          <AtlassianIcon slot="global-home" />
-          <NavLink href="/" selected>
-            <DashboardIcon slot="icon" />
-            <Link to="/">Dashboard</Link>
-          </NavLink>
-          <NavLink href="/pull-requests" >
-            <PullRequestsIcon slot="icon" />
-            <Link to="/pull-requests">Pull requests</Link>
-          </NavLink>
-          <NavLink href="/settings">
-            <GearIcon slot="icon" />
-            <Link to="/settings">Settings</Link>
-          </NavLink>
+          {
+            myLinks.map(link => (
+              <Link to={link[0]}>
+                <AkContainerItem
+                  icon={<img src="nucleus.png" alt="icon" />}
+                  text={link[1]}
+                />
+              </Link>
+            ))
+          }
         </Nav>
 
-        <div>
+        <div style={{ margin: '32px 64px' }}>
           {this.props.children}
         </div>
 
-      </Page>
+      </div>
     );
   }
 }
