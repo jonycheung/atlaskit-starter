af777c269816413278ee7a10a3ac5637d2c53260
Ben Gummer <bgummer@atlassian.com>
2017-07-24 15:02:13 -0500

    chore(deps): bump AK deps

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -1,7 +1,17 @@
+import React from 'react';
 import styled from 'styled-components';
-import { gridSizeInt } from '../constants';
+import { Grid, GridColumn } from '@atlaskit/page';
+import { akGridSizeUnitless } from '@atlaskit/util-shared-styles';
 
-export default styled.div`
-  margin: ${gridSizeInt * 4}px ${gridSizeInt * 8}px;
-  padding-bottom: ${gridSizeInt * 3}px;
+const Padding = styled.div`
+  margin: ${akGridSizeUnitless * 4}px ${akGridSizeUnitless * 8}px;
+  padding-bottom: ${akGridSizeUnitless * 3}px;
 `;
+
+export default ({ children }) => (
+  <Grid>
+    <GridColumn>
+        <Padding>{children}</Padding>
+    </GridColumn>
+  </Grid>
+)
