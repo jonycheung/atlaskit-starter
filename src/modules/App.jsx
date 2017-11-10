2a51db0bbedbee7f2c7a2b4a116b64b3cf3eb014
Raja Bellebon <rbellebon@atlassian.com>
2017-11-10 11:17:47 +1100

    remove unecessary Button

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import Button from '@atlaskit/button';
 import Flag, { FlagGroup } from '@atlaskit/flag';
 import Modal from '@atlaskit/modal-dialog';
 import Page from '@atlaskit/page';
